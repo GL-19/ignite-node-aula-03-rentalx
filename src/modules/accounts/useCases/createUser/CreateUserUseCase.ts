0f66022a1d89d37982355a4d08319ec32461d9a9
GL-19 <loiolagl19@gmail.com>
2022-01-14 15:51:03 -0300

    UserRepository criado

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,9 @@
+import { inject } from 'tsyringe';
+
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+class CreateUserUseCase {
+  constructor();
+}
+
+export { CreateUserUseCase };
