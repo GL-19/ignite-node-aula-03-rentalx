6219fd3201181525ab11a6a6e1a97eab11509601
GL-19 <loiolagl19@gmail.com>
2022-01-19 19:44:53 -0300

    Criado useCase createCar e seus testes

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,67 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
+
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepository = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepository);
+  });
+
+  it('should be able to create a new car', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car Name',
+      description: 'Car Description',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'category',
+    });
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('should not be able to create a car if license plate already exists', async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Car 1',
+        description: 'Car Description',
+        daily_rate: 100,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'category',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Car 2',
+        description: 'Car Description',
+        daily_rate: 100,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'category',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create a new car with available true by default', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car Available',
+      description: 'Car Description',
+      daily_rate: 100,
+      license_plate: 'ABCD-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'category',
+    });
+
+    expect(car).toHaveProperty('available');
+    expect(car.available).toBe(true);
+  });
+});
