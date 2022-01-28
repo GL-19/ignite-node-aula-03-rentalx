23bc37898ebb094b6e9b379665a871557203384f
GL-19 <loiolagl19@gmail.com>
2022-01-28 00:26:19 -0300

    Refatoração do código, com criação de um dateProvider

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,19 +1,25 @@
 import dayjs from 'dayjs';
 
 import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
+import { DatejsDateProvider } from '@shared/container/providers/DateProvider/implementations/DatejsDateProvider';
 import { AppError } from '@shared/errors/AppError';
 
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepository: RentalsRepositoryInMemory;
+let dayjsDateProvider: DatejsDateProvider;
 
 describe('Create Rental', () => {
   const dayAdd24Hours = dayjs().add(1, 'day').toDate();
 
   beforeEach(() => {
     rentalsRepository = new RentalsRepositoryInMemory();
-    createRentalUseCase = new CreateRentalUseCase(rentalsRepository);
+    dayjsDateProvider = new DatejsDateProvider();
+    createRentalUseCase = new CreateRentalUseCase(
+      rentalsRepository,
+      dayjsDateProvider
+    );
   });
 
   it('should be able to create a new rental', async () => {
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,11 +1,7 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
-
-dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -14,14 +10,17 @@ interface IRequest {
 }
 
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider
+  ) {}
 
   async execute({
     user_id,
     car_id,
     expected_return_date,
   }: IRequest): Promise<Rental> {
-    const minimumRentDurationHours = 24;
+    const minimumRentHoursDuration = 24;
 
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id
@@ -38,16 +37,15 @@ class CreateRentalUseCase {
     if (userHasOpenRental) {
       throw new AppError('User has a rental in progress');
     }
-    const expectedReturnDateFormat = dayjs(expected_return_date)
-      .utc()
-      .local()
-      .format();
 
-    const dateNow = dayjs().utc().local().format();
+    const dateNow = this.dateProvider.dateNow();
 
-    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, 'hour');
+    const compare = this.dateProvider.compareByHour(
+      dateNow,
+      expected_return_date
+    );
 
-    if (compare < minimumRentDurationHours) {
+    if (compare < minimumRentHoursDuration) {
       throw new AppError('Invalid return time!');
     }
 
