f7e58ee457789ee7fed7a28c79e0b93cdd98b31d
GL-19 <loiolagl19@gmail.com>
2022-05-07 21:33:40 -0300

    ensureAuthenticated refatorado, passando a utilizar refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -5,13 +5,16 @@ import { RefreshTokenUseCase } from './RefreshTokenUseCase';
 
 class RefreshTokenController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { token } = request.body;
+    const token =
+      request.body.token ||
+      request.headers['x-access-token'] ||
+      request.query.token;
 
     const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
 
-    await refreshTokenUseCase.execute(token);
+    const refresh_token = await refreshTokenUseCase.execute(token);
 
-    return response.status(200);
+    return response.json(refresh_token);
   }
 }
 
