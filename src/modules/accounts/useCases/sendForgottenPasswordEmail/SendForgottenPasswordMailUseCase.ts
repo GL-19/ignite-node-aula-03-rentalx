c82935d8c30cc01428f2a21e9f795ddc2d20b46f
GL-19 <loiolagl19@gmail.com>
2022-05-09 23:34:34 -0300

    Criado SendForgottenPasswordMailController e SendForgottenPasswordMailUseCase

--- src/modules/accounts/useCases/sendForgottenPasswordEmail/SendForgottenPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgottenPasswordEmail/SendForgottenPasswordMailUseCase.ts
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { IMailProvider } from '@shared/container/providers/MailProvider/IMailProvider';
 import { AppError } from '@shared/errors/AppError';
 
 @injectable()
@@ -14,7 +15,9 @@ class SendForgottenPasswordMailUseCase {
     @inject('UsersTokensRepository')
     private usersTokensRepository: IUsersTokensRepository,
     @inject('DayjsDateProvider')
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject('EtherealMailProvider')
+    private mailProvider: IMailProvider
   ) {}
 
   async execute(email: string): Promise<void> {
@@ -33,6 +36,12 @@ class SendForgottenPasswordMailUseCase {
       user_id: user.id,
       expiration_date,
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      'Recuperação de senha',
+      `O link para o reset é ${token}`
+    );
   }
 }
 
