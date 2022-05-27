0410203e0b4a7318ca0242068d2586fe885e7116
GL-19 <loiolagl19@gmail.com>
2022-05-27 05:49:39 -0300

    Criada rota de profile de usuário, com inclusão da url do avatar

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,6 +8,8 @@ import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 import '@shared/container';
 
+import upload from '@config/upload';
+
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
 
@@ -17,6 +19,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
