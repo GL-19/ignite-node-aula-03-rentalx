349c3bf782a7da5f8070e488d442ffb912b21b47
GL-19 <loiolagl19@gmail.com>
2022-05-31 16:46:42 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,6 +9,8 @@ import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 import '@shared/container';
 
+import cors from 'cors';
+
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
 
@@ -22,6 +24,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
