055528fc9a02814a4f33bb656e09ae55a8e6e389
GL-19 <loiolagl19@gmail.com>
2022-06-10 20:16:14 -0300

    Criado testes para CreateUserController

--- src/modules/accounts/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.spec.ts
@@ -1,7 +1,5 @@
-/* import { hash } from 'bcryptjs';
 import request from 'supertest';
 import { Connection } from 'typeorm';
-import { v4 as uuidV4 } from 'uuid';
 
 import { app } from '@shared/infra/http/app';
 import createConnection from '@shared/infra/typeorm';
@@ -14,16 +12,6 @@ describe('Create User Controller', () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
-
-    const id = uuidV4();
-
-    const password = await hash('admin', 8);
-
-    await connection.query(
-      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
-      values('${id}', 'admin', 'admin@rentalx.com.br', '${password}', true, 'now()', 'XXXXXX')
-      `
-    );
   });
 
   afterAll(async () => {
@@ -32,37 +20,38 @@ describe('Create User Controller', () => {
   });
 
   it('Should be able to create a new user', async () => {
-    const response = await request(app).post('/').send({
-      name: 'test create',
-      email: 'test_create@gmail.com',
-      password: '12345',
-      driver_license: 'XXZZ-1155',
-    });
-
-    console.log('Teste criação', response.error);
-
-    expect(response.statusCode).toBe(201);
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Glauber',
+        email: 'glauber@email.com',
+        password: '12345',
+        driver_license: '987654321',
+      })
+      .expect(201);
   });
 
-  it('Should not be able to create user with repeated email', async () => {
-    await request(app).post('/').send({
-      name: 'test email',
-      email: 'test_email@gmail.com',
-      password: '12345',
-      driver_license: 'WWAA-2755',
-    });
+  it('Should not be able to create user with a repeated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'test email',
+        email: 'test_email@gmail.com',
+        password: '12345',
+        driver_license: 'WWAA-2755',
+      })
+      .expect(201);
 
-    const response = await request(app).post('/').send({
-      name: 'test email',
-      email: 'test_email@gmail.com',
-      password: '12345',
-      driver_license: 'WWAA-2755',
-    });
-
-    console.log('Teste erro', response.error);
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'test email',
+        email: 'test_email@gmail.com',
+        password: '12345',
+        driver_license: 'WWAA-2755',
+      })
+      .expect(400);
 
     expect(response.body.message).toBe('User already exists');
-    expect(response.statusCode).toBe(400);
   });
 });
- */
